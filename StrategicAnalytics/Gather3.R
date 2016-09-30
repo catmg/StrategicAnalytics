28cb3c5b5ba0639fcabb56df457dcbe246366870
Cesar Torres <ctorres32837@gmail.com>
2016-09-30 14:55:44 -0400

    Moddile 2 SKU and HR

---
+++ StrategicAnalytics/Gather3.R
@@ -0,0 +1,12 @@
+################
+# Download CSV file from GitHub Strategic Repository
+# Cesar Torres
+# Updated September 09 2016
+# Data from "Coursera Foundations of Strategc business analytics"
+# 
+################
+
+
+
+# Read file from local Directory after GitHub Repository was replicated localy.
+data <- read.table("./StrategicAnalytics/Data/DATA_2.02_HR.csv", header = TRUE, sep = ',')
--- StrategicAnalytics/master.R
+++ StrategicAnalytics/master.R
@@ -1,18 +1,33 @@
 #Cesar Torres
-# Updated September 28 2016 January 2015
+#Updated September 28 2016 January 2015
 ################
 
 # Load knitr package
-library(knitr)
+require(knitr)
+require(markdown)
+require(ezknitr)
+
+#Clean up the memory of current R session
+rm(list = ls) = ls(all = TRUE)
 
 # Set your working directory
 setwd("C:/Users/zd6943/Desktop/GitHub/StrategicAnalytics/")
 # Gather and cleanup raw data files.
 source("./StrategicAnalytics/Gather1.R")
 
+
+#Gather dataset for Finding Groups Module 2 Example 1
+source("./StrategicAnalytics/Gather2.R")
+rmarkdown::render("./StrategicAnalytics/FindGroupsSKUModule2.rmd")
+
+#Gather dataset for Finding Groups Module 2 Example 2
+source("./StrategicAnalytics/Gather3.R")
+rmarkdown::render("./StrategicAnalytics/FindGroupsHRModule2.rmd")
+
+
+
+
 ####Execute quiz module 1
-knitr::knit2pdf(input = "./StrategicAnalytics/QuizModule1.rmd")
-source("Gather2.R")
-source("Gather3.R")
-# Merge cleaned data frames into data frame object CleanedData
-source("MergeData.R")
+#knitr::knit2pdf(input = "./StrategicAnalytics/QuizModule1.rmd")
+#rmarkdown::render("./StrategicAnalytics/QuizModule1.rmd")
+
